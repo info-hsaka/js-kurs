a2c2d99a25b6a171503e48c9b8f621b49c149818
hakizu <hakisuj@gmail.com>
2022-04-29 17:37:29 +0200

    :wrench: update share link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'HSAKA JavaScript Kurs',
   tagline: '2022',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://hakizu.xyz',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
